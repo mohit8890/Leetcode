0b862260f819b7ffe16f754922a233a31c777c5b
Mohit Yadav <96173191+mohit8890@users.noreply.github.com>
2023-11-12 21:10:09 +0530

    Update 1626-can-make-arithmetic-progression-from-sequence.cpp

--- 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.cpp
+++ 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.cpp
@@ -5,7 +5,7 @@ public:
         int n=arr.size();
         int x=arr[1]-arr[0];
         for(int i=2;i<n;i++){
-            if(arr[i]-arr[i-1]!=x)return false;
+            if(arr[i]-arr[i-1]!=x)return false
 
         }
         return true;

ce8bf6573ec4a72aa863fa80eee76ce33a2f403d
Mohit Yadav <96173191+mohit8890@users.noreply.github.com>
2023-12-05 21:40:31 +0530

    Add files via upload

---
+++ matrix.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cin>>number;
+    switch(number){
+        case 2:
+        cout<<"even number";
+        break;
+        case 4:
+        cout<<"even number";
+        break;
+        case 6:
+        cout<<"even number";
+        break;
+        case 8:
+        cout<<"even number";
+        break;
+        case 10:
+        cout<<"even number";
+        break;
+        default: 
+        cout<<"odd number";
+    }
+       
+}

7de239e68a29b50c82b261ad229090c6564172df
Mohit Yadav <96173191+mohit8890@users.noreply.github.com>
2024-02-07 11:47:18 +0530

    Add files via upload

--- 5.cpp
+++ 5.cpp
@@ -1,31 +1,41 @@
-// Error - 1 Insted of i%n we need to mention n%i. (Corrected line 16)
-// Error - 2 The code is giving wrong output for n < 2 and hence we need to mention the condition for the same. (Added code from line-9 to line-12)
+// In Line - 12, j was starting from 0 and it was printing one star more than the desired output and hence I changed it to start from j = 1 to print one star less (Corrected Line - 12)
+// In Line - 31, i++ was done and hence we were going in an infinite loop as i = size is a positive number and condition was i>0 and we were doing i++, so the condition was always true and hence I changed it i--.
 #include <iostream>
 using namespace std;
-int main()
-{
-    int n;
-    cin >> n;
-    if(n<2){
-        cout<<"Not Prime";
-        return 0;
+int main() {
+  // heart star pattern
+  int size;
+  cin>>size;
+
+  for (int i = size / 2; i < size; i += 2) {
+    // print first spaces
+    for (int j = 1; j < size - i; j += 2) { // started from j = 1
+      cout << " ";
     }
-    bool isPrime = true;
-    for (int i = 2; i < n; i++)
-    {
-        if (n % i == 0)
-        {
-            isPrime = false;
-            break;
-        }
+    // print first stars
+    for (int j = 1; j < i + 1; j++) {
+      cout << "*";
     }
-    if (isPrime)
-    {
-        cout << "Prime" << endl;
+    // print second spaces
+    for (int j = 1; j < size - i + 1; j++) {
+      cout << " ";
     }
-    else
-    {
-        cout << "Not Prime" << endl;
+    // print second stars
+    for (int j = 1; j < i + 1; j++) {
+      cout << "*";
     }
-    return 0;
+    cout << "\n";
+  }
+  // lower part
+  // inverted pyramid
+  for (int i = size; i > 0; i--) {
+    for (int j = 0; j < size - i; j++) {
+      cout << " ";
+    }
+    for (int j = 1; j < i * 2; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
 }

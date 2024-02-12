b66931339028a9ae7bfc3302f3e73bb0ac0dda34
Mohit Yadav <96173191+mohit8890@users.noreply.github.com>
2024-02-12 09:41:53 +0530

    Add files via upload

--- 3.cpp
+++ 3.cpp
@@ -1,19 +1,25 @@
-// In Line-9 we have mentioned int size = N, but there was no N declared and initialized before it, so I have initialized int N = 5; (Added Line - 8)
-// In Line-13, in the for loop it should go till <=i rather than just <i. (Corrected line - 13)
-// In Line-14, 2 stars were getting printed at a time but we just have to print a single star (Corrected line -14) 
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-  // size of the triangle
-  int N = 5;
-  int size = N;
-  // loop to print the pattern
-  for (int i = 0; i < size; i++) {
-    // print column
-    for (int j = 0; j <=i; j++) {
-      cout << "*";
+void moveNegative(int arr[], int n){
+    int i=0, j=n-1;
+    while(i<j){
+        while(arr[i]<0){ // swap when a positive element is encountered
+            i++;
+        }
+        while(arr[j]>0){ // swap when a negative element is encountered
+            j--;
+        }
+        // Additional condition to check whether i<j. This condition we have also discussed in the code of quick sort while doing the partition.
+        if(i<j)
+            swap(arr[i], arr[j]); // swap the elements
     }
-    cout << "\n";
-  }
-  return 0;
+}
+int main(){
+    int n = 5;
+    int arr[n] = {2, -3, -1, 5, -4};
+    moveNegative(arr, n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
 }

047e0bf6605df3b14c46119dc7fc17c8750e2606
Mohit Yadav <96173191+mohit8890@users.noreply.github.com>
2024-02-13 09:10:07 +0530

    Add files via upload

--- 4.cpp
+++ 4.cpp
@@ -1,14 +1,28 @@
-// Error - 1 We are not incrementing the value of j and hence we will stuck up in an infinite loop. Change ++i to ++j (Corrected line - 9)
-// Error - 2 Inside the inner for loop we need to remove \n character as it will always jump to next line and this is not a square pattern. insted of \n we add space. (Corrected line-10)
+// The meaning of the question is that we have to print those elements which are occuring more than once in the array and that too only one time.
+// So here the condition was freq[arr[i]] > 1 so it will print for frequency 2,3, and so on. So we modified the condition to freq[arr[i]] == 2 so that it prints only for 2.
 #include<iostream>
+#include<cstring>
 using namespace std;
-int main() {
-		int n;
-        cin>>n;
-        for (int i=0; i<n; ++i) {
-        for (int j=0; j<n; ++j) {
-            cout<<"* ";
+
+void printDuplicates(int arr[], int n){
+    int freq[n+1];
+    memset(freq, 0, sizeof(freq)); // initialize the frequency array to 0
+    bool flag = false; // added a flag to check if there are no duplicate elements
+    for(int i=0;i<=n;i++){
+        freq[arr[i]]++;
+        if(freq[arr[i]] == 2){
+            flag = true;
+            cout<<arr[i]<<" ";
         }
-        cout<<endl;
     }
+    if(!flag){
+        cout<<"No duplicate elements";
+    }
+}
+
+int main(){
+    int n = 6;
+    int arr[n] = {3, 2, 1, 2, 2, 3};
+    printDuplicates(arr, n);
+    return 0;
 }

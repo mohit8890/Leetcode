942567e54006fab62852c6ff09b8f49995f0dd7f
Mohit Yadav <96173191+mohit8890@users.noreply.github.com>
2024-02-15 19:48:42 +0530

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,30 @@
-// Array Index will start from 0 and not 1. The last index in the array is size - 1 where size is the number of elements in the array.
-// Change the loop from (i = 1 to i<=5) to (i = 0 to i<5) 
-#include<iostream>
+// The code was correct but there were no return statements for the recursive calls. So added those recursive calls.
+// Also the formulae of mid should be written as left + (right - left)/2 so as to avoid integer out of range problem.
+#include <iostream>
 using namespace std;
+
+int binarySearch(int arr[], int left, int right, int x) {
+    if (right >= left) {
+        // int mid = (left + right) / 2;
+        int mid = left + (right - left)/2;
+        if (arr[mid] == x)
+            return mid;
+        else if (arr[mid] > x)
+            return binarySearch(arr, left, mid - 1, x);
+        else
+            return binarySearch(arr, mid+1, right, x);
+    }
+    return -1;
+}
+
 int main() {
-   int arr[5] = {1, 2, 3, 4, 5};
-   for(int i = 0; i < 5; i++) {
-      cout << arr[i] << " ";
-   }  
-   return 0;
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 10;
+    int result = binarySearch(arr, 0, n - 1, x);
+    if (result == -1)
+        cout << "Element not found.";
+    else
+        cout << "Element found at index " << result << endl;
+    return 0;
 }

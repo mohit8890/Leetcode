5a59e15550d7bf7ac1450cfe63ff09e604f7c85c
Mohit Yadav <96173191+mohit8890@users.noreply.github.com>
2024-02-27 13:02:06 +0530

    Add files via upload

--- 2.cpp
+++ 2.cpp
@@ -1,27 +1,9 @@
-// While finding the maximum value, we need to compare with the minimum value and we took here INT_MIN
-// Consdier the example of string ssaa, here if we use max<=count[str[i]] then a would be the answer but we were suppossed to find the first maximum occuring character and hence we need to use < symbol only.
-
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
- 
-char maxOccurring(char* str)
-{
-    int count[256] = {0};
-    int max = INT_MIN; // Modification done
-    char answer;
- 
-    for (int i = 0; str[i] != '\0'; i++) {
-        count[str[i]]++;
-        if (max < count[str[i]]) { // Modification done
-            max = count[str[i]];
-            answer= str[i];
-        }
-    }
-    return answer;
-}
-
-int main()
-{
-    char str[] = "ssaa";
-    cout << maxOccurring(str);
+int main() {
+    // int* p = NULL;  --> Use dyanamic memory allocation
+    int *p = new int;
+    *p = 42;
+    delete p; // Deleting the memory taken
+    return 0;
 }

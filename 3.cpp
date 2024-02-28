2ba4f45c5ae29aba0f2db2ebf7985e416361a49b
Mohit Yadav <96173191+mohit8890@users.noreply.github.com>
2024-02-28 16:18:57 +0530

    Add files via upload

--- 3.cpp
+++ 3.cpp
@@ -1,24 +1,13 @@
-// Selection sort is based on the idea that to put the minimum element for that particular pass to its right place.
-// The logic for calculating the index of minimum element was right but then placing it at the right place was wrong.
-// So simply put swap(arr[i],arr[minIndex]) instead of swap(arr[j],arr[minIndex]) as i was taken to be the initial element and that need to be swapped with the minimum element.
-
 #include<iostream>
 using namespace std;
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1;j < n; j++) {
-            if (arr[j] < arr[minIndex])
-                minIndex = j;
-        }
-        swap(arr[i],arr[minIndex]);
-    }
+int* foo() {
+    int *p = new int; // Dyanamic memory allocation. However previous code was also working.
+    *p = 10; // Value present is changed to 10. Actualy value is updated as pointer is pointing to the actual value
+    return p;
 }
-int main(){
-    int arr[] = {5,2,4,1,3};
-    int n = 5;
-    selectionSort(arr,n);
-    for(int i =0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
+
+int main() {
+    int* q = foo();
+    cout << *q << endl; // Expected output: 10, Actual output: some random value
+    return 0;
 }

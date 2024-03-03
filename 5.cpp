4b829648a6c9f10b4bee77b2c27154e53ed40aec
Mohit Yadav <96173191+mohit8890@users.noreply.github.com>
2024-03-03 20:48:11 +0530

    Add files via upload

--- 5.cpp
+++ 5.cpp
@@ -1,12 +1,15 @@
-// The question is to find the third element means 3rd position i.e. 2nd index element
-// *(ptr + i) = arr[i];
-// Hence update *(ptr + 3) to *(ptr + 2) to find the 2nd index element
+// The base condition was edited from n < 0 to n == 0. This was observed as when I was dry running on test case 10,2
+// Ans was coming out to be 10 + 10 + 10 = 30 but we need 20 and hence we observe that the call stack was having extra function call which was not needed and hence base case was modified to avoid this function.
+
 #include<iostream>
 using namespace std;
-
+int multiplyNumbers(int m, int n) {
+    if(n == 0)
+        return 0;
+    return m + multiplyNumbers(m,n-1);
+}
 int main(){
-    int arr[5] = {1, 2, 3, 4, 5};
-    int* ptr = arr;
-    cout << "The value of the third element in arr is " << *(ptr + 2) << endl;
+    int ans = multiplyNumbers(10,2);
+    cout<<ans;
     return 0;
 }
